03827b3083cf88a2cb706cd9463522d807af8820
NicholasMaciasCarvajal <nicholas.macias@uniat.edu.mx>
2024-01-11 17:07:31 -0600

    Cree un ciclo para que se repita el proceso del login hasta que se ingrese el correcto

--- Proyecto/Login.h
+++ Proyecto/Login.h
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <fstream>
 #include <sstream>
 
 //Libreria con funciones extra de verificacion y otras funciones extra.
@@ -17,44 +17,50 @@
 std::string Login() {
     //Ingreso de usuario y contraseña
     std::string Usuario, Contrasena;
-    std::cout << "BIENVENIDO AL SISTEMA, INGRESA TU NOMBRE DE USUARIO Y CONTRASENA PARA CONTINUAR" << std::endl << "USUARIO:" << std::endl;
-    std::cin >> Usuario;
-    std::cout << "CONTRASENA:" << std::endl;
-    std::cin >> Contrasena;
 
-    std::string nombreArchivo = "BaseDeDatos.csv";
-    std::ifstream archivoEntrada(nombreArchivo);
-    if (!archivoEntrada) {
-        std::cout << "No se pudo abrir el archivo: " << nombreArchivo << std::endl;
-        return "";
-    }
+    bool verificacion = false; //cree dato para ver si se repetira el proceso del login
 
-    std::string linea;
-    bool encontrado = false;
-    while (std::getline(archivoEntrada, linea)) {
-        std::stringstream ss(linea);
-        std::string campo;
-        std::vector<std::string> campos;
+    while (!verificacion) { //while que hace que se repita el proceso del login
+        std::cout << "BIENVENIDO AL SISTEMA, INGRESA TU NOMBRE DE USUARIO Y CONTRASENA PARA CONTINUAR" << std::endl
+                  << "USUARIO:" << std::endl;
+        std::cin >> Usuario;
+        std::cout << "CONTRASENA:" << std::endl;
+        std::cin >> Contrasena;
 
-        while (std::getline(ss, campo, ',')) {
-            campos.push_back(campo);
+        std::string nombreArchivo = "BaseDeDatos.csv";
+        std::ifstream archivoEntrada(nombreArchivo);
+        if (!archivoEntrada) {
+            std::cout << "No se pudo abrir el archivo: " << nombreArchivo << std::endl;
+            return "";
         }
 
-        // Comprobación si el nombre de usuario y contraseña coinciden
-        if (campos.size() > 1 && campos[10] == Usuario && campos[11] == Contrasena) {
-            encontrado = true;
-            break;
+        std::string linea;
+        bool encontrado = false;
+        while (std::getline(archivoEntrada, linea)) {
+            std::stringstream ss(linea);
+            std::string campo;
+            std::vector<std::string> campos;
+
+            while (std::getline(ss, campo, ',')) {
+                campos.push_back(campo);
+            }
+
+            // Comprobación si el nombre de usuario y contraseña coinciden
+            if (campos.size() > 1 && campos[10] == Usuario && campos[11] == Contrasena) {
+                encontrado = true;
+                break;
+            }
+        }
+
+        archivoEntrada.close();
+
+        if (encontrado) {
+            std::cout << "Acceso concedido." << std::endl;
+            return Usuario;
+        } else {
+            std::cout << "USUARIO O CONTRASENA INCORRECTOS." << std::endl;
         }
     }
-
-    archivoEntrada.close();
-
-    if (encontrado) {
-        std::cout << "Acceso concedido." << std::endl;
-        return Usuario;
-    } else {
-        std::cout << "USUARIO O CONTRASENA INCORRECTOS." << std::endl;
-        return "";
-    }
+    return "";
 }
 #endif //PROGRAMACION1_PROYECTOPARCIAL4_LOGIN_H
