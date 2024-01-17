38adc82d91ad462a604719c009b780d34d82c423
Armando Yañez <a.leonelya@gmail.com>
2024-01-17 16:08:41 -0600

    Creamos un else para invocar la misma función y que se repita

--- CLAUDIO.md
+++ CLAUDIO.md
@@ -5,5 +5,5 @@
 <span style="color:#d41089">ENCARGADO DE CREACION DE CLASES</span>
 
 Dia 18/12/2023: Se continuo con la creación de los setters de la clase empleado y se separo las clases por cada sucursal.
-Dia 23/12/2023: Se creo la clase empleados de zapopan
-Dia 23/12/2023: Cree los geters para las clases
+Dia 23/12/2023: Se creo la clase empleados de zapopan.
+Dia 23/12/2023: Cree los geters para las clases.
--- Proyecto/Login.h
+++ Proyecto/Login.h
@@ -10,43 +10,51 @@
 
 #include <sstream>
 
+//Libreria para ingresar a los menuus.
+#include "Menus.h"
+
 //Libreria con funciones extra de verificacion y otras funciones extra.
 #include "FuncionesVerificacionesExtra.h"
 
+
 //Funcion principal login.
 void Login() {
-    //Ingreso de usuario y contraseña
-    std::string Usuario, Contrasena, Puesto;
-    std::cout << "BIENVENIDO AL SISTEMA, INGRESA TU NOMBRE DE USUARIO Y CONTRASENA PARA CONTINUAR" << std::endl << "USUARIO:" << std::endl;
-    std::cin >> Usuario;
-    std::cout << "CONTRASENA:" << std::endl;
-    std::cin >> Contrasena;
+        //Ingreso de usuario y contraseña
+        std::string Usuario, Contrasena, Puesto;
+        std::cout << "BIENVENIDO AL SISTEMA, INGRESA TU NOMBRE DE USUARIO Y CONTRASENA PARA CONTINUAR" << std::endl
+                  << "USUARIO:" << std::endl;
+        std::cin >> Usuario;
+        std::cout << "CONTRASENA:" << std::endl;
+        std::cin >> Contrasena;
 
-    std::string nombreArchivo = "BaseDeDatos.csv";
-    std::ifstream archivoEntrada(nombreArchivo);
-    if (!archivoEntrada) {
-        std::cout << "No se pudo abrir el archivo: " << nombreArchivo << std::endl;
-        return;
-    }
-
-    std::string linea;
-    bool encontrado = false;
-    while (std::getline(archivoEntrada, linea)) {
-        std::stringstream ss(linea);
-        std::string campo;
-        std::vector<std::string> campos;
-
-        while (std::getline(ss, campo, ',')) {
-            campos.push_back(campo);
+        std::string nombreArchivo = "BaseDeDatos.csv";
+        std::ifstream archivoEntrada(nombreArchivo);
+        if (!archivoEntrada) {
+            std::cout << "No se pudo abrir el archivo: " << nombreArchivo << std::endl;
+            return;
         }
 
-        // Comprobación si el nombre de usuario y contraseña coinciden
-        if (campos.size() > 1 && campos[10] == Usuario && campos[11] == Contrasena) {
-            Comparacion(campos[4]);
-            break;
-        }
-    }
-    archivoEntrada.close();
+        std::string linea;
+        bool encontrado = false;
+        while (std::getline(archivoEntrada, linea)) {
+            std::stringstream ss(linea);
+            std::string campo;
+            std::vector<std::string> campos;
 
+            while (std::getline(ss, campo, ',')) {
+                campos.push_back(campo);
+            }
+
+            // Comprobación si el nombre de usuario y contraseña coinciden
+            if (campos.size() > 1 && campos[10] == Usuario && campos[11] == Contrasena) {
+                Comparacion(campos[4]);
+                //verificacion = false;
+                break;
+            }else{
+                Login(); //se invoca la misma función en caso de que de error
+            }
+        archivoEntrada.close();
+    }
 }
+
 #endif //PROGRAMACION1_PROYECTOPARCIAL4_LOGIN_H
