978ffaa7f94e7b846222ebc2438dbc6e61d02052
Armando Yañez <a.leonelya@gmail.com>
2024-01-17 14:46:18 -0600

    Se modifico la libreria de login para hacer funcionar el menu

--- Proyecto/Login.h
+++ Proyecto/Login.h
@@ -13,8 +13,22 @@
 //Libreria con funciones extra de verificacion y otras funciones extra.
 #include "FuncionesVerificacionesExtra.h"
 
+//Funcion para comprar el puesto de los empleados.
+void Comparacion(std::string puesto){
+
+        int Eleccion;
+        if (puesto == "GERENTE GENERAL") {
+            std::cout << "bienvenido gerente" << std::endl;
+            std::cout << "que decea hacer?" << std::endl;
+            std::cout << "1. Crear usuario" << std::endl;
+            std::cout << "2. Eliminar usuario" << std::endl;
+            std::cout << "3. Editar usuario" << std::endl;
+            std::cin >> Eleccion;
+        }
+}
+
 //Funcion principal login.
-std::string Login() {
+void Login() {
     //Ingreso de usuario y contraseña
     std::string Usuario, Contrasena, Puesto;
     std::cout << "BIENVENIDO AL SISTEMA, INGRESA TU NOMBRE DE USUARIO Y CONTRASENA PARA CONTINUAR" << std::endl << "USUARIO:" << std::endl;
@@ -26,7 +40,7 @@ std::string Login() {
     std::ifstream archivoEntrada(nombreArchivo);
     if (!archivoEntrada) {
         std::cout << "No se pudo abrir el archivo: " << nombreArchivo << std::endl;
-        return "";
+        return;
     }
 
     std::string linea;
@@ -41,102 +55,12 @@ std::string Login() {
         }
 
         // Comprobación si el nombre de usuario y contraseña coinciden
-        if (campos.size() > 1 && campos[10] == Usuario && campos[11] == Contrasena && Puesto[4]) {
-            encontrado = true;
+        if (campos.size() > 1 && campos[10] == Usuario && campos[11] == Contrasena) {
+            Comparacion(campos[4]);
             break;
         }
     }
-
     archivoEntrada.close();
 
-    if (encontrado){
-        std::cout << "Acceso concedido." << std::endl;
-        std::string decea;
-
-        if (Puesto == "Gerente") {
-            std::cout << "bienvenido gerente" << std::endl;
-            std::cout << "que decea hacer?" << std::endl;
-            std::cout << "1. Crear usuario" << std::endl;
-            std::cout << "2. Eliminar usuario" << std::endl;
-            std::cout << "3. Editar usuario" << std::endl;
-            std::cin >> decea;
-
-            switch (std::stoi(decea)) {
-                case 1:
-                    std::cout << "1. Crear usuario" << std::endl;
-                    // Agrega la lógica para crear usuario aquí.
-                    break;
-                case 2:
-                    std::cout << "2. Eliminar usuario" << std::endl;
-                    // Agrega la lógica para eliminar usuario aquí.
-                    break;
-                case 3:
-                    std::cout << "3. Editar usuario" << std::endl;
-                    // Agrega la lógica para editar usuario aquí.
-                    break;
-                default:
-                    std::cout << "Opción no válida." << std::endl;
-                    break;
-            }
-        }
-        if (Puesto == "Empleado") {
-            std::cout << "bienvenido empleado" << std::endl;
-            std::cout << "que decea hacer?" << std::endl;
-            std::cout << "1. horario del empleado" << std::endl;
-            std::cout << "2. dias de descanso" << std::endl;
-            std::cout << "3. salario del empleado " << std::endl;
-            std::cin >> decea;
-
-            switch (std::stoi(decea)) {
-                case 1:
-                    std::cout << "1. horario del empleado" << std::endl;
-                    // Agrega la lógica para mostrar horario del empleado aquí.
-                    break;
-                case 2:
-                    std::cout << "2. dias de descanso" << std::endl;
-                    // Agrega la lógica para mostrar días de descanso aquí.
-                    break;
-                case 3:
-                    std::cout << "3. salario del empleado " << std::endl;
-                    // Agrega la lógica para mostrar salario del empleado aquí.
-                    break;
-                default:
-                    std::cout << "Opción no válida." << std::endl;
-                    break;
-            }
-        }
-        if (Puesto == "GerenteRecursosHumanos") {
-            std::cout << "bienvenido gerente de recursos humanos" << std::endl;
-            std::cout << "que decea hacer?" << std::endl;
-            std::cout << "1. cambiar de ubicación de empleado" << std::endl;
-            std::cout << "2. modificar el horario de un empleado" << std::endl;
-            std::cout << "3. modificar el salario del empleado" << std::endl;
-            std::cin >> decea;
-
-            switch (std::stoi(decea)) {
-                case 1:
-                    std::cout << "1. cambiar de ubicación de empleado" << std::endl;
-                    // Agrega la lógica para cambiar ubicación de empleado aquí.
-                    break;
-                case 2:
-                    std::cout << "2. modificar el horario de un empleado" << std::endl;
-                    // Agrega la lógica para modificar horario de un empleado aquí.
-                    break;
-                case 3:
-                    std::cout << "3. modificar el salario del empleado" << std::endl;
-                    // Agrega la lógica para modificar salario de un empleado aquí.
-                    break;
-                default:
-                    std::cout << "Opción no válida." << std::endl;
-                    break;
-            }
-        }
-
-        return Usuario;
-    }
-    else {
-        std::cout << "USUARIO O CONTRASENA INCORRECTOS." << std::endl;
-        return "";
-    }
 }
 #endif //PROGRAMACION1_PROYECTOPARCIAL4_LOGIN_H
